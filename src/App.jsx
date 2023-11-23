bc0a43d5bbef104323dbc3b8220d798f191b6b0a
AziziRahimSetiyawan <azizirahim04@gmail.com>
2023-11-23 23:42:32 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Beranda from "./components/Beranda";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Beranda></Beranda>
+    </>
+  );
+}
+
+export default App;
